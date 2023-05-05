b0d78a20c52ca1ba8f8d7b8c472b3b985c8dbf9d
Alexey Poletaev <av.poletaev21@gmail.com>
2023-05-05 18:26:25 +0300

    A project has been created

---
+++ GitHubSearcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubSearcher
+//
+//  Created by Alexey Poletaev on 05.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
